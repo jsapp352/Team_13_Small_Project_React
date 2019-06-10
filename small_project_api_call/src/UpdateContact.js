feb39e4ac2a68e5ffd3db5547ad8a695a71b802f
jsapp352 <32817628+jsapp352@users.noreply.github.com>
2019-06-09 22:08:52 -0400

    More refactor to fix update input box text

--- small_project_api_call/src/UpdateContact.js
+++ small_project_api_call/src/UpdateContact.js
@@ -10,6 +10,12 @@ export default class UpdateContact extends React.Component {
 
 		const user = JSON.parse(localStorage.getItem('contact'));
 		super(props);
+
+		var { show, contact, handleClose, handleSubmit } = props;
+		
+		if (contact === undefined)
+			return null;
+
 		this.state = {
 			firstName: '',
 			lastName: '',
@@ -28,12 +34,34 @@ export default class UpdateContact extends React.Component {
 			tempphone: 0,
 			tempcontactId: 0,
 			tempcreateDate: "",
-			tempdisplayName: "",			
+			tempdisplayName: "",
+			contactData: null,
 		}
 
-		// this.state.userId = user.userId;
-		// this.update = this.update.bind(this);
+		// Parse contact from API call into JSON object
+		this.state.contactData = JSON.parse(contact);
+
+		// Initialize contact info variables and corresponding tempvariables
+		console.log('Initializing tempvariables according to this JSON data:');
+		console.log(this.state.contactData);
+
+		this.state.firstName = this.state.tempfirstName = this.state.contactData.firstName;
+		this.state.lastName = this.state.templastName = this.state.contactData.lastName;
+		this.state.email = this.state.tempemail = this.state.contactData.email;
+		this.state.address = this.state.tempaddress = this.state.contactData.address;
+		this.state.phone = this.state.tempphone = this.state.contactData.phone;
+
+		console.log('New component state after tempvariable initialization:');
+		console.log(this.state);		
+
+		// Set the state variables for userId and contact Id
+		this.state.contactId = this.state.contactData.contactId;
+		this.state.userId = this.state.contactData.userId;
+
+		console.log("Component state after constructor initializations:")
 		console.log(this.state);
+
+		// Set the state variable for the handleSubmit() function reference
 		this.handleSubmit = this.handleSubmit.bind(this);
 	}
 
@@ -43,7 +71,8 @@ export default class UpdateContact extends React.Component {
 	    ReactDOM.render(<Contacts />, document.getElementById('root'))
 	}
 	  
-	handleChange(originalValue, event){				
+	handleChange(event)
+	{				
 		// If a non-null value was entered, set the state's tempvariable to that value
 		if(event.target.value !== null)
 		{
@@ -54,18 +83,15 @@ export default class UpdateContact extends React.Component {
 			});
 		}
 		// Else set the state's tempvariable to the contact property's original value
-		else
-		{
-			console.log('Setting ' + event.target.id + ' to original value (' + originalValue + ')');
+		// else
+		// {
+		// 	console.log('Setting ' + event.target.id + ' to original value (' + originalValue + ')');
 
-			this.setState({
-			[event.target.id]: originalValue
-			})
-		}
+		// 	this.setState({
+		// 	[event.target.id]: this.state.contactData[event.target.id]
+		// 	});
+		// }
 	}
-
-
-	
 
 	handleSubmit(temp)
 	{
@@ -84,33 +110,7 @@ export default class UpdateContact extends React.Component {
 		this.state.lastName = this.state.templastName;
 		this.state.email = this.state.tempemail;
 		this.state.address = this.state.tempaddress;
-		this.state.phone = this.state.tempphone;
-			
-
-		// Commented this section out because null checks already happen in handleChange
-		// And
-
-		// If any new value is empty or null, set the state variable back
-		// to its previous value.
-		// if(this.state.firstName === '' || this.state.firstName === null){
-		// 	this.state.firstName = temp.firstName;
-		// }
-
-		// if(this.state.lastName === '' || this.state.lastName === null){
-		// 	this.state.lastName = temp.lastName;
-		// }
-
-		// if(this.state.email === '' || this.state.email === null){
-		// 	this.state.email = temp.email;
-		// }
-
-		// if(this.state.phone === 0 || this.state.phone === null){
-		// 	this.state.phone = temp.phone;
-		// }
-
-		// if(this.state.address === '' || this.state.address === null){
-		// 	this.state.address = temp.address
-		// }
+		this.state.phone = this.state.tempphone;			
 
 		// Form an API request to update contact
 		console.log('Updating contact ' + this.state.contactId + '. Sending PUT request with new component state:')
@@ -133,40 +133,18 @@ export default class UpdateContact extends React.Component {
 
 	}
 
-	render() {
+	render() 
+	{
 		var { show, contact, handleClose, handleSubmit } = this.props;
-		const showHideClassName =  show  ? "pop-outer display-block" : "d-none";
-		if (contact == undefined)
-			return null;
+		const showHideClassName =  show  ? "pop-outer display-block" : "d-none";		
 
-		// Parse contact from API call into JSON object
-		const c = JSON.parse(contact);		
-
-		// IF this is first time loading the form initialize tempvariables
-		if (this.state.userId === '') 
-		{
-			console.log('Initializing tempvariables according to this JSON data:');
-			console.log(c);
-
-			this.state.tempfirstName = c.firstName;
-			this.state.templastName = c.lastName;
-			this.state.tempemail = c.email;
-			this.state.tempaddress = c.address;
-			this.state.tempphone = c.phone;
-
-			console.log('New component state after tempvariable initialization:');
-		}
-
-		// Set the state variables for userId and contact Id
-		this.state.contactId = c.contactId;
-		this.state.userId = c.userId;
-
-
-		console.log('Rendering update form for contact ' + this.state.contactId)
-		// this.fetchHelper(c);
-
+		console.log('Rendering update form for contact ' + this.state.contactId);
+		
+		
 		// Set the displayName value based on the contact JSON's name values
-		const displayName = c.firstName + " " + c.lastName
+		const c = this.state.contactData;
+		const displayName = c.firstName + " " + c.lastName;
+
 		return (
 			<div className={showHideClassName} style={{zIndex: 1}}>
 				<div className="pop-inner">
@@ -181,31 +159,31 @@ export default class UpdateContact extends React.Component {
 							<div className="form-row">
 								<div className="form-group col-md-6">
 									<label htmlFor="inputFirstN">First Name</label>
-									<input type="text" className="form-control" onChange ={(event) => this.handleChange(c.firstName, event)} id="tempfirstName" value={this.tempfirstName} />
+									<input type="text" className="form-control" onChange ={(event) => this.handleChange(event)} id="tempfirstName" value={this.tempfirstName} />
 								</div>
 								<div className="form-group col-md-6">
 									<label htmlFor="inputLastN">Last Name</label>
-									<input type="text" className="form-control" onChange ={(event) => this.handleChange(c.lastName, event)}  id="templastName" value={this.templastName} />
+									<input type="text" className="form-control" onChange ={(event) => this.handleChange(event)}  id="templastName" value={this.templastName} />
 								</div>
 							</div>
 							<div className="form-row">
 								<div className="col-sm-12">
 									<label htmlFor="inputPhone">Phone Number:</label>
-									<input type="tel" className="form-control" onChange ={(event) => this.handleChange(c.phone, event)}  id="tempphone" value={this.tempphone} />
+									<input type="tel" className="form-control" onChange ={(event) => this.handleChange(event)}  id="tempphone" value={this.tempphone} />
 								</div>
 							</div>
 							<br />
 							<div className="form-row">
 								<div className="col-sm-12">
 									<label htmlFor="inputEmail">Email:</label>
-									<input type="email" className="form-control" onChange ={(event) => this.handleChange(c.email, event)} id="tempemail" value={this.tempemail} />
+									<input type="email" className="form-control" onChange ={(event) => this.handleChange(event)} id="tempemail" value={this.tempemail} />
 								</div>
 							</div>
 							<br />
 							<div className="form-row">
 								<div className="col-sm-12">
 									<label htmlFor="inputAddress">Address:</label>
-									<input type="address" className="form-control" onChange ={(event) => this.handleChange(c.address, event)} id="tempaddress" value={this.tempaddress} />
+									<input type="address" className="form-control" onChange ={(event) => this.handleChange(event)} id="tempaddress" value={this.tempaddress} />
 								</div>
 							</div>
 							<br />
